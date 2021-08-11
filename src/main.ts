24e955737bd5c1cc3d50c09aa9554a8c74d87d83
Alzwimn <alzwimn@gmail.com>
2021-08-11 17:52:50 -0300

    feat: finish save new user route

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true
+      whitelist: true,
     }),
   );
   await app.listen(3000);
