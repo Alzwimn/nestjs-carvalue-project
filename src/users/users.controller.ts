857c43dfa43312e4ac929a31aa5936ed26ea7af0
Alzwimn <alzwimn@gmail.com>
2021-08-11 22:16:55 -0300

    feat: add find methods to user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dtos/create-user.dto';
 import { UsersService } from 'src/users/users.service';
 
@@ -9,5 +9,15 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string) {
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get('')
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
   }
 }
