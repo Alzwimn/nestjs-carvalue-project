eb0d0ff218a634d3b10e712fe71f9ff9ae045370
Alzwimn <alzwimn@gmail.com>
2021-08-11 22:10:52 -0300

    feat: add remove method to usersServices

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -30,7 +30,10 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  remove() {
+  async remove(id: number) {
+    const user = await this.repo.findOne(id);
+    if (!user) throw new Error('User not found');
 
+    return this.repo.remove(user);
   }
 }
