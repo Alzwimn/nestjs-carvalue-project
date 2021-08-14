d90f79039a31291f326869108ed0caae0f35c2ea
Alzwimn <alzwimn@gmail.com>
2021-08-13 21:33:10 -0300

    feat: create currentUser Interceptor

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -4,7 +4,8 @@ import {
 } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContext) => {
-    return 'hiiiiiieeee';
+  (data: never, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.currentUser;
   }
 );
