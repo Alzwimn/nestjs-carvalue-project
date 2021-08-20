bad4df95da8ec60eb323fcc6f4b0e4a65dc1d84e
Alzwimn <alzwimn@gmail.com>
2021-08-20 20:54:04 -0300

    test: create auth service template test

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test } from '@nestjs/testing';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from 'src/users/auth.service';
+import { User } from 'src/users/user.entity';
+
+it('can create an instance of auth service', async () => {
+  const fakeUsersService: Partial<UsersService> = {
+    find: () => Promise.resolve([]),
+    create: (email: string, password: string) =>
+      Promise.resolve({ id: 1, email, password } as User),
+  };
+
+  const module = await Test.createTestingModule({
+    providers: [
+      AuthService,
+      {
+        provide: UsersService,
+        useValue: fakeUsersService,
+      },
+    ],
+  }).compile();
+
+  const service = module.get(AuthService);
+
+  expect(service).toBeDefined();
+});
