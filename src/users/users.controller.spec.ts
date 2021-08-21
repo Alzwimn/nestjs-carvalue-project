8d7061a1f31d3be021ad414f74097888e20c3b0e
Alzwimn <alzwimn@gmail.com>
2021-08-20 22:42:48 -0300

    test(UsersController): module should be defined

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from 'src/users/users.service';
+import { AuthService } from 'src/users/auth.service';
+import { User } from 'src/users/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+
+    fakeUsersService = {
+      findOne: (id: number) => {
+        return Promise.resolve({ id, email: 'test@email.com', password: 'passwd'} as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'password'} as User]);
+      },
+      // remove: () => {},
+      // update: () => {},
+    };
+
+    fakeAuthService = {
+      // signin: (email: string, password: string) => {},
+      // signup: (email: string, password: string) => {}
+    };
+
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
