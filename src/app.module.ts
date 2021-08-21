3e7b8e953516e3a8b99c25aa22f446b4b163643b
Alzwimn <alzwimn@gmail.com>
2021-08-21 12:54:47 -0300

    chrore(main): use environment variables to setup diferent configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,24 @@ import { ReportsModule } from './reports/reports.module';
 import { User } from 'src/users/user.entity';
 import { Report } from 'src/reports/report.entity';
 const cookieSession = require('cookie-session');
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true
+        }
+      }
     }),
     UsersModule,
     ReportsModule,
